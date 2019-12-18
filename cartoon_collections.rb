cbe4eccc9cbbdb4927f973e96db90bfe07106840
Anthony Pardo <pardo.anthony@gmail.com>
2019-12-18 02:53:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,14 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  i = 0 
+  while i < calls.length do 
+    if calls[i].length > 4 
+      return true 
+    end
+  end
+  false
 end
 
 def find_the_cheese# code an argument here
